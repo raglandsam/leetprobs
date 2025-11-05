3fd781d51a3364edf81fca4adb5435b1c3aaaf46
Samuel Ragland <161576907+raglandsam@users.noreply.github.com>
2025-11-05 21:08:18 +0530

    Time: 61 ms (15.24%), Space: 57.6 MB (14.49%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var val=init;
+    return {
+        increment : function() {
+            val++;
+            return val;
+        } ,
+        decrement : function(){
+            val--;
+            return val;
+        },
+        reset : function() {
+            val = init;
+            return val;
+        }
+    }
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
