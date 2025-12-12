07d5acc958935f576ab15353c062e5657cab8690
Samuel Ragland <161576907+raglandsam@users.noreply.github.com>
2025-12-12 12:49:57 +0530

    Time: 11 ms (45.37%), Space: 13.2 MB (92.28%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -2,16 +2,16 @@ class Solution(object):
     def asteroidCollision(self, asteroids):
         stack=[]
         for i in asteroids :
-            rightsmall = True
+            ITSALIVE = True
             while stack and ((i<0) and (0<stack[-1])):
                 if abs(i) > abs(stack[-1]):
                     stack.pop()
                     continue
                 if abs(i) ==abs(stack[-1]):
                     stack.pop()
-                rightsmall= False
+                ITSALIVE= False
                 break
-            if rightsmall:
+            if ITSALIVE:
                 stack.append(i)
         return stack
  
