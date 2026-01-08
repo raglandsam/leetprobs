53788c87e6d9f11cded76483e364a1e1057bd9c8
Samuel Ragland <161576907+raglandsam@users.noreply.github.com>
2026-01-08 16:44:04 +0530

    Time: 4 ms (50.36%), Space: 13.4 MB (31.14%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        d={}
+        for i in range(len(nums)):
+            diff=target-nums[i]
+            if diff in d:
+                return [d[diff],i]
+            d[nums[i]]=i
+             
