f17cd544854083f53d5ee9bbf649b04d7660e1d1
Samuel Ragland <161576907+raglandsam@users.noreply.github.com>
2026-01-13 19:12:44 +0530

    Time: 95 ms (70.86%), Space: 19.5 MB (5.4%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -4,12 +4,9 @@ class Solution(object):
         l=[]
         for i in points:
             euc=(i[0]*i[0]+i[1]*i[1])
-            arr.append([euc,i[0],i[1]])
+            arr.append([euc,[i[0],i[1]]])
         arr=sorted(arr , key= lambda x:x[0])
-        for i in range(k):
-            l.append([arr[i][1],arr[i][2]])
-        return l
-
+        return [arr[i][1] for i in range(k)]
 
 
 
