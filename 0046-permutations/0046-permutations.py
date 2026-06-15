260ca6dc8aee8a7304bfb9c7bbcce095f443ba4c
Samuel Ragland <161576907+raglandsam@users.noreply.github.com>
2026-06-15 11:37:27 +0530

    Time: 0 ms (100%), Space: 12.5 MB (56.18%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def permute(self, nums):
+        n=len(nums)
+        if  n==1:
+            return [nums]
+        result=[]
+        def backtrack(vals,path):
+            if len(path)==n:
+                result.append(path[:])
+                return
+            for i in nums:
+                if i in path:
+                    continue
+                path.append(i)
+                backtrack(i,path)
+                path.pop()
+        backtrack(nums,[])
+        return result
+
+
