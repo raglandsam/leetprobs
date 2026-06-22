26eae37a31322010ec29cb47e232df2376162078
Samuel Ragland <161576907+raglandsam@users.noreply.github.com>
2026-06-22 18:18:52 +0530

    Time: 0 ms (100%), Space: 12.4 MB (53.91%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def combinationSum3(self, k, n):
+        arr=[i for i in range(1,10)]
+        l=len(arr)
+        result=[]
+        sum=0
+        def find_comb(index,path,sum):
+            #BP1, func parameters change
+            if sum>n or len(path)>k:
+                    return
+            if sum==n and len(path)==k:
+                result.append(path[:])
+                return
+            for i in range(index,l):
+        
+                path.append(arr[i])
+                sum+=arr[i]
+                
+                find_comb(i+1,path,sum)
+                sum-=path.pop()
+        find_comb(0,[],sum)
+        return result
