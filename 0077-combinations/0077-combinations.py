3b968c8a1a781f3be396a0dc4c76dd76562f2b51
Samuel Ragland <161576907+raglandsam@users.noreply.github.com>
2026-06-26 14:47:01 +0530

    Time: 171 ms (93.5%), Space: 130.9 MB (91.66%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def combine(self, n, k):
+        result=[]
+        def back_track(index,path):
+            if len(path)==k :
+                result.append(path[:])
+                return
+            for i in range(index,n-(k-len(path))+2):
+                path.append(i)
+                back_track(i+1,path)
+                path.pop()
+        back_track(1,[])
+        return result
+            
+
+
+        
