c6d17ab46201b29505b7bfe06e236279b58a188f
Samuel Ragland <161576907+raglandsam@users.noreply.github.com>
2026-07-07 18:40:32 +0530

    Time: 0 ms (100%), Space: 12.3 MB (50.84%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findComplement(self, num):
+        n=num
+        r=''
+        while n>0:
+            r+=str(n%2)
+            n//=2
+        res=0
+        for i in range(len(r)):
+            if r[i]=='0':
+                res+=1*(2**i)
+            
+        return int(res)
+
+
+        
+        
+
+
+
+
+        
