0432dc04572c45b6fa1fa125649864c38ae8e97d
Samuel Ragland <161576907+raglandsam@users.noreply.github.com>
2026-07-09 18:47:05 +0530

    Time: 0 ms (100%), Space: 12.3 MB (52.25%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.py
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.py
@@ -1,16 +1,8 @@
 class Solution(object):
     def isSameAfterReversals(self, num):
-        if num==0:
+        if num%10==0 and num!=0:
+            return False
+        else:
             return True
-        x=num
-        res1=0
-        while num>0:
-            res1=res1*10 + num%10
-            num//=10
-        res2=0
-        while res1>0:
-            res2=res2*10 + res1%10
-            res1//=10
-        return x==res2
         
         
