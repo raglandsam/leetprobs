cfdb5a3b94d0a3a4bba40851b71fba4ff1dc049d
Samuel Ragland <161576907+raglandsam@users.noreply.github.com>
2026-09-25 10:44:28 +0530

    Time: 7 ms (98.32%), Space: 22.4 MB (41.71%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,10 @@
-from collections import defaultdict
-class Solution(object):
-    def groupAnagrams(self, strs):
-        d=defaultdict(list)
-        for s in strs:
-            t=tuple(sorted(s))
-            d[t].append(s)
-        return d.values()
-
-
-            
-
-        
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        d={}
+        for word in strs:
+            tup=tuple(sorted(word))
+            if tup in d:
+                d[tup].append(word)
+            else:
+                d[tup]=[word]
+        return list(d.values())
